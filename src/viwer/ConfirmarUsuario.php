ad0b97e82f57834f1113bc906b35582bfbbbfe7c
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2025-10-29 18:43:56 -0300

    versao 1.0

---
+++ src/viwer/ConfirmarUsuario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/ConfirmarUsuario.css">
+    <title>ConfirmarUsuario</title>
+</head>
+<body>
+    <header id="top">
+        <h1 id="title">RadarVôlei</h1>
+    </header>
+</body>
+</html>
