005bf5c9af26b0b22f54ab099d3f9b0eb3b3ba4b
Carlos2326-jpg <https://github.com/Carlos2326-jpg/Trabalho02.Eletiva>
2025-11-13 16:22:36 -0300

    versao backup

--- src/processos/Conexao.php
+++ src/processos/Conexao.php
@@ -2,11 +2,10 @@
 $hostName = "localhost";
 $bancoDados = 'radarvolei';
 $usuario = "root";
-$senha = "";
+$senha = "123";
 
 $mySql = new mysqli($hostName, $usuario, $senha, $bancoDados);
 
 if ($mySql->connect_errno) {
     echo "<h1>Falha ao conectar: (" . $mySql->connect_errno . ") " . $mySql->connect_error . "</h1>";
 }
-?>
